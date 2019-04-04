1b98a3ec999f0abc2f11318eeba0cec68997ffbf
hhhuy <hhuuyy95@gmail.com>
2019-04-04 14:56:44 +0700

    ung dung product discount calculator

---
+++ productDiscountCalculator.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>Product Discount Calculator</title>
+    <style>
+        #content {
+            width: 450px;
+            margin: 0 auto;
+            padding: 0px 20px 20px;
+            background: white;
+            border: 2px solid navy;
+        }
+
+        h1 {
+            color: navy;
+        }
+
+        label {
+            width: 10em;
+            padding-right: 1em;
+            float: left;
+        }
+
+        #data input {
+            float: left;
+            width: 15em;
+            margin-bottom: .5em;
+        }
+    </style>
+</head>
+<body>
+
+<div id="content">
+    <h1>Product Discount Calculator</h1>
+    <form method="post" action="display.php">
+        <div id="data">
+            <label>Product Description:</label>
+            <input type="text" name="description"/><br/>
+            <label>List Price:</label>
+            <input type="text" name="price"/><br/>
+            <label>Discount Percent:</label>
+            <input type="text" name="discount_percent"/><div style="padding-top: 20px">%</div><br/>
+        </div>
+        <div id="buttons">
+            <label>&nbsp;</label>
+            <input type="submit" value="Calculate Discount"/>
+        </div>
+    </form>
+</div>
+</body>
+</html>
